945d9eda4b3d0a09351d80dd3993c35d7164c04f
codexiaozhu <861353958@qq.com>
2024-01-02 16:39:48 +0800

    fix: 修复eslint检测bug

--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,10 @@
-const { isProd, isPrdDev } = require("../utils/env")
+const { isProd, isPrdDev } = require('../utils/env');
 
 // 获取各个环境的不同配置文件
 let fileName = 'dev.js';
-if (isProd) fileName = 'prd_env.js'
-if (isPrdDev) fileName = 'prd.js'
+if (isProd) fileName = 'prd_env.js';
+if (isPrdDev) fileName = 'prd.js';
 
-const conf = require(`./envs/${fileName}`)
+const conf = require(`./envs/${fileName}`);
 
-module.exports = conf
+module.exports = conf;
--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,21 +1,21 @@
 const mongoose = require('mongoose');
 const { mongodbConf } = require('../config/index');
 
-const { host, port, dbName } = mongodbConf
+const { host, port, dbName } = mongodbConf;
 
 let url = `mongodb://${host}:${port}`;
 
-mongoose.connect(`${url}/${dbName}`)
+mongoose.connect(`${url}/${dbName}`);
 
-const db = mongoose.connection
+const db = mongoose.connection;
 
-db.on('error', err => {
-    console.error('mongoose connect error', err)
-})
+db.on('error', (err) => {
+    console.error('mongoose connect error', err);
+});
 
 // 执行 node src/db/mongoose.js 测试连接
 db.once('open', () => {
     console.log('mongoose connect success');
-})
+});
 
-module.exports = mongoose
+module.exports = mongoose;
